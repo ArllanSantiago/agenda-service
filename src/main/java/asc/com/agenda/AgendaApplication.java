5b823c665c0105c6184b407e52844bef51e0675a
ArllanFelipe Santigo Correia <arllansantiago@gmail.com>
2023-09-22 04:41:21 -0300

    commit inicial

---
+++ src/main/java/asc/com/agenda/AgendaApplication.java
@@ -0,0 +1,13 @@
+package asc.com.agenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaApplication.class, args);
+	}
+
+}
